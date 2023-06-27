fa53b1bc2e9c530009f1da0bcdc32324a68e3720
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-06-27 23:19:11 +0300

    add new exercise

---
+++ 2.Components/articles-app/src/app/data/data.ts
@@ -0,0 +1,41 @@
+// import {data  } from "../data/seed";
+// import { Article } from "../models/article.model";
+
+
+import { Article } from "../models/article.model";
+import { data } from "./seed";
+
+export class ArticleData {
+    getData(): Article[] {
+        let articles: Article[] = [];
+        data;
+        for (let i = 0; i < data.length; i++) {
+            articles[i] = new Article(data[i].title, data[i].description, data[i].author, data[i].imageUrl)
+        }
+        return articles;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
